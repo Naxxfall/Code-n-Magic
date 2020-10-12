ab82c4a3b4ffdf37d0129024e4c17745731eac8f
Serj <pedro8@list.ru>
2020-10-12 16:50:07 +0300

    Задание 2 (Начало)

---
+++ js/setup.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var setupBlock = document.querySelector(".setup");
+var wizardNames = ["Иван", "Хуан Себастьян", "Мария", "Кристоф", "Виктор", "Юлия", "Люпита", "Вашингтон"];
+var wizardSurnames = ["да Марья", "Верон", "Мирабелла", "Вальц", "Онопко", "Топольницкая", "Нионго", "Ирвинг"];
+var coatColors = ["rgb(101, 137, 164)", "rgb(241, 43, 107)", "rgb(146, 100, 161)", "rgb(56, 159, 117)", "rgb(215, 210, 55)", "rgb(0, 0, 0)"];
+var eyesColors = ["black", "red", "blue", "yellow", "green"];
+
+function generateWizard (names, surnames, coatColors, eyesColors){
+  var wizard = {
+    name: names[Math.floor(Math.random() * names.length)],
+    surname: surnames[Math.floor(Math.random() * surnames.length)],
+    coatColor: coatColors[Math.floor(Math.random() * coatColors.length)],
+    eyesColor: eyesColors[Math.floor(Math.random() * eyesColors.length)]
+  };
+  return wizard;
+}
+
+setupBlock.classList.remove("hidden");
+
+
